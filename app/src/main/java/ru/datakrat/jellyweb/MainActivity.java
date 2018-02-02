95661a319b810d1aab46d606e32a0064fb87c443
ArturB <bad@datakrat.ru>
2018-02-02 17:55:43 +0500

    add Web3dsActivity

--- app/src/main/java/ru/datakrat/jellyweb/MainActivity.java
+++ app/src/main/java/ru/datakrat/jellyweb/MainActivity.java
@@ -1,13 +1,84 @@
 package ru.datakrat.jellyweb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private static final int PICK_3DS_REQUEST = 1;
+
+    TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button onWebButton = (Button) findViewById(R.id.main_on_web_button);
+        onWebButton.setOnClickListener(this);
+
+        resultTextView = (TextView) findViewById(R.id.main_result_text_view);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_on_web_button:
+                onWeb();
+                break;
+        }
+    }
+
+    private void onWeb() {
+        String acsUrl = "https://web.rbsuat.com/acs/auth/start.do";
+        String mdOrder = "b7f03c47-39ed-7fd5-b7f0-3c4700002d0c";
+        String paReq = "eJxVkttygjAQhl/F4R5zEFGcNY7HqTPVSsULL2OIwiioEVrK0zdRqPVuv83m382/gUGRnBpfUt3ic9q3SBNbDZmKcxinh761CWZ21xowCCIl5WQtRa4kg4W83fhBNuKwb2F33xKeF9qUOJ7teF7H3oU7z96LnXT3AmNMOhaD1fBTXhlUjZju06SAatSKSkQ8zRhwcR3Nl8yhHRdjQBVCItV8wghtOW230wX0YEh5IlnIM35UPAN0RxDnPM3UD3OdFqAaIFcnFmXZpYdQfaGpckAmD+g5wCo30U3rFHHIFhO/WJbTYlFOySIQZFlu6EdwbOu4D8hUgFaTjGLSxRTTBqE9p9sjHqB7HnhiBmAtbYR5zoPgYpoMX47+p0D7rPQa6jfUBLK4nFOpK7R5fzGg58zjN2OhyLQ5kV/MROR/h8F74m9n1/W0dMaj62pbfHFj7L3IKMbaHuKRh6QBQEYGVTtD1bp19PINfgFeu7tR";
+        String termUrl = "https://web.rbsuat.com/ab/rest/finish3ds.do";
+
+        String encMdOrder = null;
+        String encPaReq = null;
+        String encTermUrl = null;
+        try {
+            encMdOrder = URLEncoder.encode(mdOrder, "UTF-8");
+            encPaReq = URLEncoder.encode(paReq, "UTF-8");
+            encTermUrl = URLEncoder.encode(termUrl, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        Intent intentWeb = new Intent(this, Web3dsActivity.class);
+        intentWeb.putExtra(Web3dsActivity.ASC_URL, acsUrl);
+        intentWeb.putExtra(Web3dsActivity.MD_ORDER, encMdOrder);
+        intentWeb.putExtra(Web3dsActivity.PA_REQ, encPaReq);
+        intentWeb.putExtra(Web3dsActivity.TERM_URL, encTermUrl);
+        startActivityForResult(intentWeb, PICK_3DS_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case PICK_3DS_REQUEST:
+                String pares = null;
+                if (resultCode == RESULT_OK) {
+                    if (data != null) {
+                        if (data.getStringExtra("PARES") != null) {
+                            pares = data.getStringExtra("PARES");
+                        }
+                    }
+                } else if (resultCode == RESULT_CANCELED) {
+                    pares = "Operation cancelled";
+                }
+                resultTextView.setText(pares);
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
---
+++ app/src/main/java/ru/datakrat/jellyweb/Web3dsActivity.java
@@ -0,0 +1,176 @@
+package ru.datakrat.jellyweb;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.JsResult;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class Web3dsActivity extends Activity {
+    public static final String TAG = "Web3dsActivity";
+
+    private WebView mWebView;
+    private ProgressBar mProgressBar;
+
+    public static final String REDIRECT_FORM = "redirect_form";
+    public static final String ASC_URL = "acs_url";
+    public static final String MD_ORDER = "md_order";
+    public static final String PA_REQ = "pa_req";
+    public static final String TERM_URL = "term_url";
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_3ds);
+        if (getActionBar() != null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        mProgressBar = (ProgressBar) findViewById(R.id.web_3ds_progress_bar);
+        mProgressBar.setMax(100);
+        mWebView = (WebView) findViewById(R.id.web_3ds_web_view);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true); // this lines did not solve 'net::ERR_PROXY_CONNECTION_FAILED' problem
+        mWebView.setWebViewClient(new BarsWebViewClient());
+        mWebView.setWebChromeClient(new BarsWebChromeClient());
+
+        Intent intent = getIntent();
+        String redirectForm = intent.getStringExtra(REDIRECT_FORM);
+        String acsUrl = intent.getStringExtra(ASC_URL);
+        String mdOrder = intent.getStringExtra(MD_ORDER);
+        String paReq = intent.getStringExtra(PA_REQ);
+        String termUrl = intent.getStringExtra(TERM_URL);
+
+        if (redirectForm != null) {
+            mWebView.loadData(redirectForm, "text/html", "en_US");
+        } else if (mdOrder != null && paReq != null && termUrl != null) {
+            String postData = "MD=" + mdOrder + "&PaReq=" + paReq + "&TermUrl=" + termUrl;
+            mWebView.postUrl(acsUrl, postData.getBytes());
+        } else {
+            mWebView.loadUrl(acsUrl);
+        }
+    }
+
+    private class BarsWebViewClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            Log.d(TAG,"onPageStarted: " + url + ", Parameters: " + view.getOriginalUrl());
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            Log.d(TAG,"onReceivedError, errorCode: " + errorCode + ", description: " + description + ", fallingUrl: " + failingUrl);
+            onError();
+        }
+
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            Log.d(TAG,"onReceivedSslError: " + error.toString());
+            if (error.hasError(2) || error.hasError(3)) {
+                handler.proceed(); // Ignore SSL certificate errors
+            } else {
+                handler.cancel();
+            }
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            Log.d(TAG,"onPageFinished: " + url);
+            Web3dsActivity.this.setTitle(getString(R.string.secure_3ds));
+            if (view.getTitle() != null) {
+                if (!view.getTitle().contains("html")) {
+                    Web3dsActivity.this.setTitle(view.getTitle());
+                }
+            }
+            super.onPageFinished(view, url);
+
+            if (url.contains("PaRes")){
+                Intent intent = new Intent();
+                String paRes = Uri.parse(url).getQueryParameter("PaRes");
+                Log.d(TAG,"Success, PaRes=" + paRes);
+                intent.putExtra("PARES", paRes);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+
+            if (url.contains("Cancel") || url.contains("cancel")){
+                Log.d(TAG,"Cancel");
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        }
+    }
+
+    private void onError() {
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setTitle(R.string.error_title)
+                .setMessage(R.string.error_payment_message)
+                .setCancelable(false)
+                .setNegativeButton(android.R.string.yes, (dialog, whichButton) -> dialog.cancel())
+                .create();
+        alertDialog.show();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class BarsWebChromeClient extends WebChromeClient {
+        @Override
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+            Log.d(TAG,"onJsAlert, message: " + message);
+            result.confirm();
+            return true;
+        }
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            if (newProgress == 100) {
+                mProgressBar.setVisibility(View.GONE);
+            } else {
+                mProgressBar.setVisibility(View.VISIBLE);
+                mProgressBar.setProgress(newProgress);
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (mWebView.canGoBack()) {
+                    mWebView.goBack();
+                } else {
+                    finish();
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
